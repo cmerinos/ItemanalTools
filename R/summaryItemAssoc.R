0d9127c99044f0f7638213e8b723c4ed902e7b95
Cesar Merino-Soto <sikayax@yahoo.com.ar>
2025-05-16 23:08:58 -0600

    ...

--- R/summaryItemAssoc.R
+++ R/summaryItemAssoc.R
@@ -11,21 +11,27 @@
 #' @return A data frame with summary statistics (mean, median) for the coefficient and CIs (if given).
 #'
 #' @examples
+#' result <- data.frame(
+#'   Item = c("Item1", "Item2", "Item3"),
+#'   rg = c(.21, .35, .28),
+#'   lwr.ci = c(.12, .22, .15),
+#'   lwr.upp = c(.31, .45, .38)
+#' )
 #' summaryItemAssoc(df = result, coef.col = "rg", lwr.col = "lwr.ci", upr.col = "lwr.upp")
 #'
 #' @export
-summaryItem <- function(df, coef.col, lwr.col = NULL, upr.col = NULL) {
+summaryItemAssoc <- function(df, coef.col, lwr.col = NULL, upr.col = NULL) {
   out <- data.frame(
     coef.mean = mean(df[[coef.col]], na.rm = TRUE),
     coef.median = median(df[[coef.col]], na.rm = TRUE)
   )
-  
+
   if (!is.null(lwr.col) && !is.null(upr.col)) {
     out$lwr.mean <- mean(df[[lwr.col]], na.rm = TRUE)
     out$lwr.median <- median(df[[lwr.col]], na.rm = TRUE)
     out$upr.mean <- mean(df[[upr.col]], na.rm = TRUE)
     out$upr.median <- median(df[[upr.col]], na.rm = TRUE)
   }
-  
+
   return(round(out, 3))
 }
